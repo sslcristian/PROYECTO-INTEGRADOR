b40ee72e5a34e8a28465cc325c22ff4c477789a0
crist <crist@LAPTOP-KH51J1NO>
2025-05-04 16:41:20 -0500

    NUEVO AVANCE CRISTIAN

--- proyectoIntegrador/src/controller/UserMenuController.java
+++ proyectoIntegrador/src/controller/UserMenuController.java
@@ -4,12 +4,12 @@ import application.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
+
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+
 import model.Session;
 import model.Usuario;
 import javafx.scene.control.ButtonBar;
